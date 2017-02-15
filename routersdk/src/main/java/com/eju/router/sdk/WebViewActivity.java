fd130a9e752e6ecaaa78db680a9e356ad6ee9e0f
zhangjun <zhangjun@ehousechina.com>
2017-02-15 18:18:13 +0800

    little fix.

--- routersdk/src/main/java/com/eju/router/sdk/WebViewActivity.java
+++ routersdk/src/main/java/com/eju/router/sdk/WebViewActivity.java
@@ -1,17 +1,23 @@
 package com.eju.router.sdk;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
 import com.eju.router.sdk.exception.EjuException;
 
 import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 /**
  * The {@code WebViewActivity} is used to display html resource.
- *
+ * <p>
  * BTW, this class is also the default html parameter handler.
  * it's used to handle parameter that in url like 'http:\\aa.com?a=1' and take these parameter
  * into html contents.
@@ -19,8 +25,8 @@ import java.util.ArrayList;
  * @author SidneyXu
  */
 
-public class WebViewActivity extends Activity implements HtmlHandler {
-
+public class WebViewActivity extends AppCompatActivity implements HtmlHandler, BridgeHandler {
+    private static final String TAG = WebViewActivity.class.getSimpleName();
     public static final String EXTRA_URL = "_url";
 
     private static final String END_HTML = "</html>";
@@ -37,12 +43,20 @@ public class WebViewActivity extends Activity implements HtmlHandler {
 
 
     private ProgressWebView webView;
+    private Router router;
+    //    private ActionBar actionBar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        webView = getWebView();
-        setContentView(webView);
+//        webView = getWebView();
+        setContentView(R.layout.activity_web);
+        webView = (ProgressWebView) findViewById(R.id.web);
+        getWebView();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("这不是标题");
 
         handleIntent();
     }
@@ -53,17 +67,25 @@ public class WebViewActivity extends Activity implements HtmlHandler {
      * @return an instance of com.eju.router.sdk.ProgressWebView
      */
     protected ProgressWebView getWebView() {
-        webView = new ProgressWebView(this);
+//        webView = new ProgressWebView(this);
         webView.disableSideEffect();
         webView.getRemoteInterceptor().setParamHandler(this);
         webView.getLocalInterceptor().setParamHandler(this);
-        webView.setDefaultHandler(new DefaultHandler(this));
+        webView.setDefaultHandler(this);
+        webView.registerHandler("ToolBarJs", new BridgeHandler() {
+            @Override
+            public void handler(BridgeWebView webView, String url, CallBackFunction function) {
+                if (TextUtils.equals("toggleToolBar", url)) {
+                    toolbar.setVisibility(toolbar.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+                }
+            }
+        });
         webView.send("Hello ，Kugou");
         return webView;
     }
 
     private void load(String url) {
-        if(null == url) {
+        if (null == url) {
             return;
         }
 
@@ -100,20 +122,20 @@ public class WebViewActivity extends Activity implements HtmlHandler {
         String html = new String(contents);
 
         int i = html.lastIndexOf(END_HTML);
-        if(-1 == i) {
+        if (-1 == i) {
             throw new EjuException(String.format("[%s] has wrong html format !", url));
         }
 
         int length = html.length();
-        if(i + END_HTML.length() > length) {
+        if (i + END_HTML.length() > length) {
             html = html.substring(0, i + END_HTML.length());
         }
 
         Bundle bundle = getIntent().getExtras();
         StringBuilder builder = new StringBuilder();
-        for(String key : bundle.keySet()) {
+        for (String key : bundle.keySet()) {
             // ignore '_url' parameter
-            if(key.equalsIgnoreCase(EXTRA_URL)) {
+            if (key.equalsIgnoreCase(EXTRA_URL)) {
                 continue;
             }
             builder.append(key).append(':').append(parseObjectOfJS(bundle.get(key)))
@@ -130,22 +152,22 @@ public class WebViewActivity extends Activity implements HtmlHandler {
     private String parseObjectOfJS(Object object) {
         StringBuilder builder = new StringBuilder();
 
-        if(null == object) {
+        if (null == object) {
             builder.append("null");
             return builder.toString();
         }
 
         Class<?> clazz = object.getClass();
-        if(String.class.isAssignableFrom(clazz)) {
+        if (String.class.isAssignableFrom(clazz)) {
             builder.append('"').append(object).append('"');
-        } else if(ArrayList.class.isAssignableFrom(clazz)
+        } else if (ArrayList.class.isAssignableFrom(clazz)
                 || clazz.isArray()) {
             builder.append('[');
-            for(Object o : ((ArrayList)object)) {
+            for (Object o : ((ArrayList) object)) {
                 builder.append(parseObjectOfJS(o)).append(',');
             }
             builder.append(']');
-        } else if(Boolean.class.isAssignableFrom(clazz)
+        } else if (Boolean.class.isAssignableFrom(clazz)
                 || Byte.class.isAssignableFrom(clazz)
                 || Character.class.isAssignableFrom(clazz)
                 || Short.class.isAssignableFrom(clazz)
@@ -158,10 +180,10 @@ public class WebViewActivity extends Activity implements HtmlHandler {
             builder.append('{');
 
             Field[] fields = clazz.getDeclaredFields();
-            for(Field field : fields) {
+            for (Field field : fields) {
                 // ignore {@code this} field.
                 // Is there better solution to this ?
-                if(field.getDeclaringClass() != clazz
+                if (field.getDeclaringClass() != clazz
                         || field.getName().matches(".*this.*")) {
                     continue;
                 }
@@ -170,11 +192,37 @@ public class WebViewActivity extends Activity implements HtmlHandler {
                     builder.append(field.getName()).append(':')
                             .append(parseObjectOfJS(field.get(object)));
                     builder.append(',');
-                } catch (IllegalAccessException ignored) {}
+                } catch (IllegalAccessException ignored) {
+                }
             }
             builder.append('}');
         }
 
         return builder.toString();
+    }
+
+    @Override
+    public void handler(BridgeWebView webView, String url, CallBackFunction function) {
+        Log.e(TAG, "handler: " + url);
+        EjuLog.d("shouldOverrideUrlLoading");
+        if (function != null) {
+            function.onCallBack("DefaultHandler response data:" + url);
+        }
+        if (url.startsWith("http://") || url.startsWith("https://")) {
+            webView.loadUrl(url);
+            return;
+        }
+        if (null == router) {
+            router = Router.getInstance();
+        }
+        if (router.isNativeRouteSchema(url)) {
+            try {
+                URI uri = new URI(url);
+                router.internalRoute(this, uri);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                router.broadcastException(new EjuException(EjuException.UNKNOWN_ERROR, e.getMessage()));
+            }
+        }
     }
 }
