073645fb48251e6b288fb851016e96081c50dd22
Tang Qianwei <tangqianwei@ehousechina.com>
2017-02-17 10:29:38 +0800

    temp commit

--- app/src/main/java/com/ejurouter/router_sdk/App.java
+++ app/src/main/java/com/ejurouter/router_sdk/App.java
@@ -1,10 +1,13 @@
 package com.ejurouter.router_sdk;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.eju.router.sdk.EjuRequest;
+import com.eju.router.sdk.HtmlHandler;
 import com.eju.router.sdk.Option;
 import com.eju.router.sdk.Router;
+import com.eju.router.sdk.exception.EjuException;
 import com.ejurouter.router_sdk.activity.DefaultActivity;
 
 /**
@@ -19,12 +22,23 @@ public class App extends Application {
         Router router = Router.getInstance();
         Option option = new Option();
         option.defaultVersion = "V1.0.0";
-        option.request = EjuRequest.newBuilder()
-                .url("http://172.29.32.215:10086/app/checkViewMap?appName=demo&os=android")
-                .method(EjuRequest.METHOD_GET)
-                .build();
+//        option.request = EjuRequest.newBuilder()
+//                .url("http://172.29.32.215:10086/app/checkViewMap?appName=demo&os=android")
+//                .method(EjuRequest.METHOD_GET)
+//                .build();
         router.initialize(this, option);
         router.set404ViewMap(DefaultActivity.class.getName());
+
+        router.addHtmlHandlerWithUrl(".*172\\.29\\.32\\.215:8080.*", new HtmlHandler() {
+            @Override
+            public byte[] handle(String url, byte[] contents) throws EjuException {
+                Log.d("EjuLog", new String(contents));
+                return contents;
+            }
+        });
+
         router.registerPageNeedNativeParameter(".*172\\.29\\.32\\.215:8080.*");
+        router.registerPageNeedNativeParameter("^file.+");
+        router.registerPageNeedNativeParameter("^eju.+");
     }
 }
